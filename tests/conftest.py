c98bd27cce5d11f9dee45ff3356a56ab34526632
mehran8023@gmail.com <mehran8023@gmail.com>
2026-08-30 14:58:24 +0000

    conftest files modify

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "databricks"))
+
 import pytest
 from pyspark.sql import SparkSession
 
